b0ad9e57d83794f32271d9ef3c20427ef78c4f8b
Beinan Wang <soros.wang@yahoo.com>
2014-10-15 23:36:40 -0400

    add asm 5.0 to do the bytecode injection

--- weaving/build.sbt
+++ weaving/build.sbt
@@ -5,3 +5,7 @@ version := "1.0"
 scalaVersion := "2.10.3"
 
 libraryDependencies += "org.ow2.asm" % "asm" % "5.0.3"
+
+libraryDependencies += "org.ow2.asm" % "asm-analysis" % "5.0.3"
+
+libraryDependencies += "org.ow2.asm" % "asm-util" % "5.0.3"
--- weaving/src/main/scala/edu/syr/jbd/weaving/JBDClassFileTransformer.scala
+++ weaving/src/main/scala/edu/syr/jbd/weaving/JBDClassFileTransformer.scala
@@ -1,17 +1,41 @@
 package edu.syr.jbd.weaving
 
 import java.lang.instrument.ClassFileTransformer
-
 import java.security.ProtectionDomain
+
+import org.objectweb.asm._
+import org.objectweb.asm.util.CheckClassAdapter
 
 class JBDClassFileTransformer extends ClassFileTransformer{
 
-	def transform(loader : ClassLoader, className : String, 
-		classBeingRefined: Class[_], 
-		protectionDomain: ProtectionDomain,
-		classFileBuffer: Array[Byte]):Array[Byte] = {
+  def transform(loader : ClassLoader, className : String, 
+    classBeingRefined: Class[_], 
+    protectionDomain: ProtectionDomain,
+    classFileBuffer: Array[Byte]):Array[Byte] = {
 
-		println(className)
-		classFileBuffer
-	}
+    
+    val normalizedClassName = className.replaceAll("/", "\\.");
+    println(normalizedClassName)
+
+    val classReader = new ClassReader(classFileBuffer)
+    val classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS)
+    val classVisitor = new TraceClassAdapter(classWriter)
+    classReader.accept(new CheckClassAdapter(classVisitor), ClassReader.EXPAND_FRAMES)
+    return classWriter.toByteArray
+  }
+}
+
+class TraceClassAdapter(cv: ClassVisitor) extends ClassVisitor(Opcodes.ASM4, cv) with Opcodes {
+  override def visit(version: Int, access: Int, name: String, signature: String, superName: String, interfaces: Array[String]) {
+    owner = name
+    println("class-visited:" + name)
+    super.visit(version, access, name, signature, superName, interfaces)
+  }
+  override def visitMethod(access: Int, name: String, desc: String, signature: String, exceptions: Array[String]): MethodVisitor = {
+    println("method-visited:" + name)
+    val mv: MethodVisitor = cv.visitMethod(access, name, desc, signature, exceptions)
+    //return if (mv == null) null else new TraceMethodAdapter(api, mv, owner, access, name, desc)
+    return mv
+  }
+  private var owner: String = null
 }
