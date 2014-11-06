be340b4c51f5870b3febc59f1f084bdcce990513
Beinan <bwang19@syr.edu>
2014-11-06 15:02:34 -0500

    load config from mongodb

---
+++ tracing/src/main/scala/edu/syr/jbd/tracing/TraceConfig.scala
@@ -0,0 +1,49 @@
+package edu.syr.jbd.tracing
+
+import reactivemongo.bson._
+import reactivemongo.api._
+    
+import edu.syr.jbd.tracing.mongo.MongoDB
+
+
+object TraceConfig{
+  import scala.concurrent.Await
+  import scala.concurrent.duration._
+  lazy val default:ConfigDoc = Await.result(ConfigDoc.load_default, 2000 millis)  
+
+}
+
+case class ConfigDoc(
+  symbol: String,
+  includedClasses: List[String],
+  excludedClasses: List[String]
+  )
+
+
+
+object ConfigDoc{
+  import scala.concurrent.ExecutionContext.Implicits.global
+  def load_default = MongoDB.coll("config")
+  .find(BSONDocument("symbol" -> "default"))
+  .one[ConfigDoc](ConfigDocReader,scala.concurrent.ExecutionContext.Implicits.global).map{ //IMPROVE ME!!!
+    case Some(item) => item
+    case _ => ConfigDoc("default", Nil, Nil)
+  }
+
+
+  implicit object ConfigDocWriter extends BSONDocumentWriter[ConfigDoc] {
+    def write(doc: ConfigDoc): BSONDocument = BSONDocument(
+      "symbol" -> doc.symbol,
+      "includedClasses" -> doc.includedClasses,
+      "excludedClasses" -> doc.excludedClasses)
+  }
+
+  implicit object ConfigDocReader extends BSONDocumentReader[ConfigDoc] {
+    def read(doc: BSONDocument): ConfigDoc = ConfigDoc(
+      doc.getAs[String]("symbol").get,
+      doc.getAs[List[String]]("includedClasses").toList.flatten,
+      doc.getAs[List[String]]("excludedClasses").toList.flatten)
+  } 
+}
+
+
---
+++ tracing/src/main/scala/edu/syr/jbd/tracing/mongo/MongoDB.scala
@@ -0,0 +1,39 @@
+package edu.syr.jbd.tracing.mongo
+
+object MongoDB{
+	import reactivemongo.api._
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+
+    // gets an instance of the driver
+    // (creates an actor system)
+    val driver = new MongoDriver
+    val connection = driver.connection(List("localhost"))
+
+    // Gets a reference to the database "plugin"
+    val db = connection("jbd")
+
+    def coll(coll_name: String) = db(coll_name)
+
+    def test = {
+    	import reactivemongo.bson._
+        import scala.util.{Success, Failure}
+
+    	val document = BSONDocument(
+          "symbol" -> "default",  
+		  "includeClasses" -> List(""),
+          "excludedClasses" -> List("com.sun.","sun.")
+          )
+
+    	val future = coll("config").insert(document)
+    	future.onComplete {
+		  case Failure(e) => throw e
+		  case Success(lastError) => {
+		    println("successfully inserted document with lastError = " + lastError)
+            connection.close()
+		  }
+		}
+        
+    }
+ }
+
